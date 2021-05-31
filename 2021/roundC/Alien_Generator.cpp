ec8eaa4169452dfe8cef60289c38393a1ebffa87
Dishank Goel <dishankgoel1729@gmail.com>
2021-05-31 13:17:36 +0530

    Add 2021 problems

--- 2021/roundC/Alien_Generator.cpp
+++ 2021/roundC/Alien_Generator.cpp
@@ -3,24 +3,21 @@
 
 using namespace std;
 
-ll test_case() {
-    ll r,c; cin>>r>>c;
-    int grid[r][c];
-    for(int i = 0; i < r; i++) {
-        for(int j = 0; j < c; j++) {
-            cin>>grid[i][j];
-        }
-    }
-    
-}
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;cin>>t;
     for(int case_no = 0; case_no < t; case_no++) {
-        ll ans = test_case();
+        ll g; cin>>g;
+
+        ll ans = 0;
+        for(int i  = 1; i <= (int)pow(2*g, 0.5); i++) {
+            if((2*g)%i == 0) {
+                if((((2*g)/i) - i + 1)%2 == 0) {
+                    ans++;
+                }
+            }
+        }
         cout<<"Case #"<<case_no+1<<": "<<ans<<"\n";
     }
 }
